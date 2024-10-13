a2d6833c7370f6aafcf2cbb7762244885b33fe32
MinhBang1000 <bradonleminhbang1@gmail.com>
2024-10-13 12:27:02 +0800

    Submit homework 4 and 5

--- Homework 5/Project/313540015.js
+++ Homework 5/Project/313540015.js
@@ -1,11 +1,220 @@
-import { dropdownMenu } from "./dropdownMenu.js"
-import { stackedBarChart } from "./stackedBarChart.js"
-
 const {
   select,
   selectAll,
-  csv
+  csv,
+  stack,
+  scaleLinear,
+  scaleBand,
+  scaleOrdinal,
+  axisBottom,
+  axisLeft,
+  schemeCategory10,
+  map
 } = d3;
+
+// Dropdown Component
+const dropdownMenu = (
+  selection,
+  props
+) => {
+  const {
+    options,
+    onOptionClicked,
+    selectedOption,
+  } = props;
+
+  let select = selection
+    .selectAll('select')
+    .data([null]);
+  select = select
+    .enter()
+    .append('select')
+    .merge(select)
+    .on('change', function () {
+      onOptionClicked(this.value);
+    });
+
+  const option = select
+    .selectAll('option')
+    .data(options);
+
+  // Remove any options that are no longer needed
+  option.exit().remove();
+  option
+    .enter()
+    .append('option')
+    .merge(option)
+    .attr('value', (d) => d)
+    .property(
+      'selected',
+      (d) => d === selectedOption
+    )
+    .text((d) => d);
+};
+
+// Stack Bar Charts Creation
+const stackedBarChart = (selection,props) => {
+  const {
+    margin,
+    width,
+    height,
+    data,
+    subgroups,
+    colorLegendLabel,
+  } = props;
+
+  const innerWidth =
+    width - margin.right - margin.left - 50;
+  const innerHeight =
+    height - margin.top - margin.bottom - 200;
+
+  const g = selection
+    .selectAll('.container')
+    .data([null]);
+  const gEnter = g
+    .enter()
+    .append('g')
+    .attr('class', 'container');
+  gEnter
+    .merge(g)
+    .attr(
+      'transform',
+      `translate(${margin.left},${margin.top})`
+    );
+
+  let groups = map(data, function (d) { // Name of university
+      return d.name;
+    })
+    .keys();
+
+  const xScale = scaleBand()
+    .domain(groups)
+    .range([0, innerWidth])
+    .padding([0.2]);
+
+  const yScale = scaleLinear()
+    .domain([0, 100])
+    .range([innerHeight, 0]);
+
+  const colorScale = scaleOrdinal()
+    .domain([
+      'teaching',
+      'research',
+      'citations',
+      'industry income',
+      'international outlook'
+    ])
+    .range(schemeCategory10);
+
+  const xAxisG = g.select('.x-axis');
+  const xAxisGEnter = gEnter
+    .append('g')
+    .attr('class', 'x-axis');
+  xAxisG
+    .merge(xAxisGEnter)
+    .attr(
+      'transform',
+      `translate(0,${innerHeight})`
+    )
+    .call(
+      axisBottom(xScale)
+        .tickPadding(10)
+        .tickSizeOuter(0)
+    )
+    .selectAll('text')
+    .style('text-anchor', 'start')
+    .attr('transform', 'rotate(45)');
+
+  xAxisG.merge(xAxisGEnter).selectAll('.tick text')
+    .style('font-size', '14px')
+
+  const yAxisG = g.select('.y-axis');
+  const yAxisGEnter = gEnter
+    .append('g')
+    .attr('class', 'y-axis');
+  yAxisG
+    .merge(yAxisGEnter)
+    .call(axisLeft(yScale).tickPadding(10));
+  
+  yAxisG.merge(yAxisGEnter).style('font-size', '14px')
+
+  let stackedData = stack().keys(subgroups)(
+    data
+  );
+
+  const tooltip = select('body')
+    .append('div')
+    .attr('class', 'tooltip')
+    .style('opacity', 0); // Initially hidden
+
+  //add bars
+  const bars = g
+    .merge(gEnter)
+    .selectAll('rect')
+    .data(stackedData);
+  bars
+    .enter()
+    .append('g')
+    .attr('fill', (d) => colorScale(d.key))
+    .selectAll('rect')
+    .data((d) => d)
+    .enter()
+    .append('rect')
+    .attr('class', d => d)
+    .attr('x', function (d) {
+      return xScale(d.data.name);
+    })
+    .attr('y', function (d) {
+      return yScale(d[1]);
+    })
+    .attr('height', function (d) {
+      return yScale(d[0]) - yScale(d[1]);
+    })
+    .attr('width', xScale.bandwidth())
+    .on('mouseover', function (d) {
+      // Show the tooltip
+      tooltip
+        .transition()
+        .duration(200)
+        .style('opacity', 0.9);
+
+      // Define the content of the tooltip
+      tooltip.html(
+        `<div><strong>Name:</strong> ${d.data.name}</div>` +
+          `<div><strong>Overall:</strong> ${d.data['scores_overall']}</div>` +
+          `<div><strong>Teaching:</strong> ${d.data['scores_teaching']}</div>` +
+          `<div><strong>Research:</strong> ${d.data['scores_research']}</div>` +
+          `<div><strong>Citations:</strong> ${d.data['scores_citations']}</div>` +
+          `<div><strong>Industry Income:</strong> ${d.data['scores_industry_income']}</div>` +
+          `<div><strong>International Outlook:</strong> ${d.data['scores_international_outlook']}</div>`
+      );
+    })
+    .on('mousemove', function (d) {
+      select(this).classed('hovered', true)
+      tooltip
+        .style('left', d3.event.pageX + 10 + 'px')
+        .style('top', d3.event.pageY - 30 + 'px');
+    })
+    .on('mouseout', function () {
+      select(this).classed('hovered', false)
+      // Hide the tooltip
+      tooltip
+        .transition()
+        .duration(500)
+        .style('opacity', 0);
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
 
 const svg = select('svg');
 
@@ -161,19 +370,10 @@ const render = () => {
       (currentPage - 1) * itemsPerPage;
     const end = start + itemsPerPage;
     const pageData = sortedData.slice(start, end);
+
     // Update stacked bar chart with the pageData
-
-    // let subgroups = [
-    //   'teaching',
-    //   'research',
-    //   'citations',
-    //   'industry income',
-    //   'international outlook',
-    // ];
-
     //remove before update
     svg.selectAll('*').remove();
-    // console.log(pageData)
     svg.call(stackedBarChart, {
       margin: {
         top: 30,
@@ -219,7 +419,7 @@ const render = () => {
 };
 
 csv(
-  'TIMES_WorldUniversityRankings_2024.csv'
+  'http://vis.lab.djosix.com:2024/data/TIMES_WorldUniversityRankings_2024.csv'
 ).then((loadedData) => {
   data = loadedData.filter((d) => {
     return d.rank !== 'Reporter';
---
+++ Homework 5/Project/README.md
@@ -0,0 +1,25 @@
+# Visualizing World University Rankings 2024: A Stacked Bar Chart Approach - Homework 5
+
+## Structure
+The project consists of the following files:
+- `313540015.html`: The main HTML file that serves as the entry point for the web application.
+- `313540015.js`: The JavaScript file that contains the logic for data visualization and interaction.
+- `313540015.css`: The CSS file for styling the application.
+- `data.csv`: The data file used for visualization.
+
+## Run
+To run the application, open the `313540015.html` file using Live Server or a local server. This will allow you to view the application in your web browser without encountering CORS issues.
+
+### Instructions
+1. **Live Server**: If you are using Visual Studio Code, you can install the Live Server extension. Right-click on `313540015.html` and select "Open with Live Server."
+2. **Local Server**: Alternatively, you can set up a local server using Python or Node.js:
+   - **Python**: Run the following command in the terminal within your project directory:
+     ```bash
+     python -m http.server 8000
+     ```
+   - **Node.js**: Install `http-server` globally and run:
+     ```bash
+     http-server
+     ```
+
+After starting the server, access your application at `http://localhost:8000` (or the appropriate address for your server setup).
